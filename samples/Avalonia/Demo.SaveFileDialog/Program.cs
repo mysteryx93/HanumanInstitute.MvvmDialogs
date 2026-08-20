90c886818ce66ff4bd116f737788ed22bb21611b
Dimitris Trachiotis <acemod13@outlook.com>
2026-08-20 21:00:44 +0300

    Update packages. Add Wayland support for Avalonia projects.

--- samples/Avalonia/Demo.SaveFileDialog/Program.cs
+++ samples/Avalonia/Demo.SaveFileDialog/Program.cs
@@ -18,5 +18,6 @@ class Program
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .LogToTrace()
+            .UseWaylandWithFallback()
             .UseReactiveUI(rxui => { });
 }
